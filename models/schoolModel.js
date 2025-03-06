51533a6b224f68e28d8a1709bdf598505670d929
sobhit1 <sobhitraghav1092@gmail.com>
2025-03-06 22:30:02 +0530

    Changed database table name in schoolModel.js

--- models/schoolModel.js
+++ models/schoolModel.js
@@ -2,13 +2,13 @@ const db = require('../config/db');
 
 const addSchool = (name, address, latitude, longitude) => {
     return db.query(
-        'INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)',
+        'INSERT INTO schools_data (name, address, latitude, longitude) VALUES (?, ?, ?, ?)',
         [name, address, latitude, longitude]
     );
 };
 
 const getAllSchools = () => {
-    return db.query('SELECT * FROM schools');
+    return db.query('SELECT * FROM schools_data');
 };
 
 module.exports = { addSchool, getAllSchools };
